7c795d0037bb55460f3b3fccad008c0642d08435
Urpagin <passieuxmattis@gmail.com>
2023-04-21 09:07:21 +0200

    added /database method to API and changed the way ip and key are checked

--- main.py
+++ main.py
@@ -17,25 +17,46 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 load_dotenv()
 HASHED_PASSWORD = os.getenv('HASHED_PASSWORD')
-MACHINES_FILENAME = 'machines.db'
+MACHINES_FILENAME = 'machines3.db'
 
 if not HASHED_PASSWORD:
     raise Exception('HASHED_PASSWORD is empty.')
 
 
-async def check_ip_key(ip: str, key: str) -> None:
+async def verify_key(key: str) -> None:
     if hashlib.sha384(key.encode('utf-8')).hexdigest() != HASHED_PASSWORD:
-        raise HTTPException(detail='Error, key is invalid',
+        raise HTTPException(detail='Error, key invalid',
                             status_code=status.HTTP_401_UNAUTHORIZED)
-    elif not ip.isascii():
-        raise HTTPException(detail='Error, ip is invalid - not ascii',
-                            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
-    elif not ip:
+
+
+async def verify_ip(ip: str) -> None:
+    if not ip:
         raise HTTPException(detail='Error, ip is null',
                             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
-    elif len(ip) > 15:
-        raise HTTPException(detail='Error, ip is too long (15 chars max)',
+    if not ip.isascii():
+        raise HTTPException(detail='Error, ip is not ASCII',
                             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    if len(ip) > 15:
+        raise HTTPException(detail='Error, ip too long (15 chars max)',
+                            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    if len(ip) < 7:
+        raise HTTPException(detail='Error, ip too short (7 chars min)',
+                            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    ip_segments: list[str, ...] = ip.split('.')
+    if len(ip_segments) != 4:
+        raise HTTPException(detail='Error, ip must have 4 segments',
+                            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    try:
+        ip_segments: list[int, int, int, int] = [int(seg) for seg in ip_segments]
+    except ValueError:
+        raise HTTPException(detail='Error, ip not made of integers',
+                            status_code=status.HTTP_401_UNAUTHORIZED)
+
+    for seg in ip_segments:
+        if seg > 255 or seg < 0:
+            raise HTTPException(detail='Error, ip malformed',
+                                status_code=status.HTTP_401_UNAUTHORIZED)
 
 
 @app.get('/')
@@ -47,7 +68,8 @@ async def read_root():
 
 @app.post('/wake')
 async def wake(ip: str, key: str):
-    await check_ip_key(ip, key)
+    await verify_key(key)
+    await verify_ip(ip)
     try:
         await wake_machine(ip, MACHINES_FILENAME)
     except Exception as e:
@@ -59,7 +81,8 @@ async def wake(ip: str, key: str):
 
 @app.get('/ping')
 async def ping(ip: str, key: str):
-    await check_ip_key(ip, key)
+    await verify_key(key)
+    await verify_ip(ip)
     try:
         ping_resp = await _ping(ip)
     except Exception as e:
@@ -75,7 +98,8 @@ async def ping(ip: str, key: str):
 
 @app.put('/add-machine')
 async def add_machine(ip: str, key: str):
-    await check_ip_key(ip, key)
+    await verify_key(key)
+    await verify_ip(ip)
     add_machine_obj = AddMachine(MACHINES_FILENAME)
 
     mac = getmac.get_mac_address(ip=ip)
@@ -87,6 +111,13 @@ async def add_machine(ip: str, key: str):
         return {'detail': f'Error: {e}'}
     else:
         return {'detail': f'Successfully added {ip} in database'}
+
+
+@app.get('/database')
+async def database(key: str):
+    await verify_key(key)
+    add_machine_obj = AddMachine(MACHINES_FILENAME)
+    return {'detail': add_machine_obj.get_all_rows()}
 
 
 if __name__ == "__main__":
